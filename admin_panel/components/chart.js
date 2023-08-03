e905cdb209b7f0f688c6da02c04b0d6132e14511
Saif-Ul-llah <102151531+Saif-Ul-llah@users.noreply.github.com>
2023-08-03 22:07:51 +0500

    next

--- admin_panel/components/chart.js
+++ admin_panel/components/chart.js
@@ -2,7 +2,25 @@
 //import React, { useEffect, useState } from 'react';
 
 
-import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+function Example(props) {
+  return (
+    <div style={{ marginBottom: 80 ,marginLeft:40}}>
+      <hr style={{ border: "2px solid #ddd" }} />
+      <div style={{ marginTop: 30}}>
+        <div style={{ width: "350", paddingRight: 30 }}>{props.children}</div>
+        <div style={{ width: "350" }}>
+          <h3 className="h5">{props.label}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 const Chart = () => {
 
@@ -11,7 +29,7 @@ const chartData = [
     { name: 'Category 2', value: 3 },
     { name: 'Category 3', value: 20 },
     { name: 'Category 1', value: 17 },
-  ];
+];
 
   const userChart = [
     { name: 'Category 1', value: 30 },
@@ -24,6 +42,8 @@ const chartData = [
     { name: 'Category 2', value: 70 },
     
   ];
+
+  const percentage = 88;
 
   const COLORS = ['gray','#ADD8E6','#191970','#FF6384', '#36A2EB', '#FFCE56', '#FF5733', '#00E874']; // Add more colors as needed
   const renderLabel = (entry) => {
@@ -68,10 +88,23 @@ const chartData = [
             </Pie>
            
           </PieChart>
+          
         </div>
+        <div className='mr-20'>
+          <h2 className='text-2xl font-bold mb-4 text-center'>Demo</h2>
+          <Example>
+            <CircularProgressbar
+              value={percentage}
+              text={`${percentage}%`}
+              strokeWidth={5}
+            />
+           </Example>
+          </div>
       </div>
     </>
   );
 };
 
 export default Chart;
+
+
